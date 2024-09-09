f7b2d44d2eb57cb3bffa9084f2037706c54d9a43
pritishvaidya <pritishvaidya94@gmail.com>
2024-09-09 21:54:35 +0530

    Init Cover agent file

--- src/tasks/findRelatedTests.ts
+++ src/tasks/findRelatedTests.ts
@@ -24,7 +24,14 @@ const findSimilarNamedFiles = async (filePath: string): Promise<string[]> => {
         // Find files with similar names
         const similarFiles = files.filter((file) => {
             const fileBaseName = path.basename(file, path.extname(file));
-            console.log("file paths", file, fileBaseName);
+            console.log(
+                "file paths",
+                file,
+                fileBaseName,
+                path.basename(filePath),
+                fileBaseName.includes(baseName) &&
+                    file !== path.basename(filePath),
+            );
             return (
                 fileBaseName.includes(baseName) &&
                 file !== path.basename(filePath)
